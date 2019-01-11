762e7be04e4abb92e2280de4c47af9ccb0aa991c
capellev <capellev.info@gmail.com>
2019-01-11 15:45:43 +0100

    Create a copy of seq_ with 22 extra value reprensenting circular DNA & fix uses in DNA.cpp

--- Dna.cpp
+++ Dna.cpp
@@ -6,6 +6,7 @@
 #include "ExpManager.h"
 
 Dna::Dna(const Dna& clone) : seq_(clone.seq_) {
+  createSeqMod();
 }
 
 Dna::Dna(int length, Threefry::Gen& rng) : seq_(length) {
@@ -13,13 +14,18 @@ Dna::Dna(int length, Threefry::Gen& rng) : seq_(length) {
   for (int32_t i = 0; i < length; i++) {
     seq_[i] = '0' + rng.random(NB_BASE);
   }
+
+  createSeqMod();
 }
 
 Dna::Dna(char* genome, int length) : seq_(length) {
   strcpy(seq_.data(), genome);
+
+  createSeqMod();
 }
 
 Dna::Dna(int length) : seq_(length) {
+  createSeqMod();
 }
 
 int Dna::length() const {
@@ -37,15 +43,45 @@ void Dna::load(gzFile backup_file) {
   gzread(backup_file,&dna_length,sizeof(dna_length));
 
   gzread(backup_file, seq_.data(), sizeof(*seq_.data()));
+
+  createSeqMod();
+}
+
+void Dna::createSeqMod() {
+  seqMod_ = seq_;
+  seqMod_.resize(seq_.size() + 22);
+
+  for (int i = 0; i < 22; i ++) {
+    seqMod_[seqMod_.size() + i] = seq_[i];
+  }
 }
 
 void Dna::set(int pos, char c) {
   seq_[pos] = c;
+  seqMod_[pos] = c;
+
+  if (pos < 22) {
+    seqMod_[seq_.size() + pos] = c;
+  }
 }
 
 void Dna::do_switch(int pos) {
-  if (seq_[pos] == '0') seq_[pos] = '1';
-  else seq_[pos] = '0';
+  if (seq_[pos] == '0') {
+    seq_[pos] = '1';
+    seqMod_[pos] = '1';
+
+  if (pos < 22) {
+    seqMod_[seq_.size() + pos] = '1';
+  }
+  }
+  else {
+    seq_[pos] = '0';
+    seqMod_[pos] = '0';
+
+  if (pos < 22) {
+    seqMod_[seq_.size() + pos] = '0';
+  }
+  }
 }
 
 
@@ -56,12 +92,7 @@ int Dna::promoter_at(int pos) {
     // Searching for the promoter
     prom_dist[motif_id] =
         PROM_SEQ[motif_id] ==
-        seq_[
-            pos + motif_id >= seq_.size() ? pos +
-                                            motif_id -
-                                            seq_.size()
-                                          : pos +
-                                            motif_id]
+        seqMod_[pos + motif_id ]
         ? 0
         : 1;
 
@@ -101,17 +132,8 @@ int Dna::terminator_at(int pos) {
 
     // Search for the terminators
     term_dist[motif_id] =
-        seq_[
-            pos + motif_id >= seq_.size() ? pos +
-                                            motif_id -
-                                            seq_.size() :
-            pos + motif_id] !=
-        seq_[
-            pos - motif_id + 10 >= seq_.size() ?
-            pos - motif_id + 10 - seq_.size() :
-            pos -
-            motif_id +
-            10] ? 1
+        seqMod_[pos + motif_id] !=
+        seqMod_[pos - motif_id + 10] ? 1
                 : 0;
   }
   int dist_term_lead = term_dist[0] +
@@ -128,11 +150,9 @@ bool Dna::shine_dal_start(int pos) {
 
   for (int k = 0; k < 9; k++) {
     k_t = k >= 6 ? k + 4 : k;
-    t_pos = pos + k_t >= seq_.size() ? pos + k_t -
-                                       seq_.size()
-                                     : pos + k_t;
+    t_pos = pos + k_t;
 
-    if (seq_[t_pos] ==
+    if (seqMod_[t_pos] ==
         SHINE_DAL_SEQ[k]) {
       start = true;
     } else {
@@ -149,11 +169,9 @@ bool Dna::protein_stop(int pos) {
   int t_k;
 
   for (int k = 0; k < 3; k++) {
-    t_k = pos + k >= seq_.size() ?
-          pos - seq_.size() + k :
-          pos + k;
+    t_k = pos + k;
 
-    if (seq_[t_k] ==
+    if (seqMod_[t_k] ==
         PROTEIN_END[k]) {
       is_protein = true;
     } else {
@@ -171,11 +189,8 @@ int Dna::codon_at(int pos) {
   int t_pos;
 
   for (int i = 0; i < 3; i++) {
-    t_pos =
-        pos + i >= seq_.size() ? pos + i -
-                                 seq_.size()
-                               : pos + i;
-    if (seq_[t_pos] ==
+    t_pos = pos + i;
+    if (seqMod_[t_pos] ==
         '1')
       value += 1 << (CODON_SIZE - i - 1);
   }
